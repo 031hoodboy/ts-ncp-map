3d3147e7dd5d9eea34c455fdb0ee2cdda70eff9e
031hoodboy <littlepark0107@gmail.com>
2022-09-13 14:00:57 +0900

    feat: add marker click

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,21 @@ function App() {
   const mapTypeIdList = ["normal", "terrain", "satellite", "hybrid"];
 
   const [mapTypeId, setMapTypeId] = useState("normal");
+
   useEffect(() => {
     const { naver } = window;
     if (!mapElement.current || !naver) return;
 
     // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
-    const location = new naver.maps.LatLng(37.5656, 126.9769);
+    const location = new naver.maps.LatLng(
+      37.49450980317381,
+      127.01270976617246
+    );
     const mapOptions: naver.maps.MapOptions = {
       center: location,
-      zoom: 17,
+      zoom: 14,
+      // maxZoom: 14,
+      // minZoom: 14,
       zoomControl,
       keyboardShortcuts: keyboardShortcuts,
       zoomControlOptions: {
@@ -31,49 +37,16 @@ function App() {
       draggable: draggable,
       mapTypeId: mapTypeId,
     };
+    console.log(mapOptions.zoom?.toExponential);
     const map = new naver.maps.Map(mapElement.current, mapOptions);
-    new naver.maps.Marker({
-      position: location,
-      map,
+
+    var marker = new naver.maps.Marker({
+      position: new naver.maps.LatLng(37.49450980317381, 127.01270976617246),
+      map: map,
     });
 
-    var polygon = new naver.maps.Polygon({
-      map: map,
-      paths: [
-        [
-          new naver.maps.LatLng(37.5656, 126.9769),
-          new naver.maps.LatLng(37.37230584065902, 127.10791110992432),
-          new naver.maps.LatLng(37.35975408751081, 127.10795402526855),
-          new naver.maps.LatLng(37.359924641705476, 127.11576461791992),
-          new naver.maps.LatLng(37.35931064479073, 127.12211608886719),
-          new naver.maps.LatLng(37.36043630196386, 127.12293148040771),
-          new naver.maps.LatLng(37.36354029942161, 127.12310314178465),
-          new naver.maps.LatLng(37.365211629488016, 127.12456226348876),
-          new naver.maps.LatLng(37.37544345085402, 127.11224555969238),
-        ],
-      ],
-      fillColor: "#ff0000",
-      fillOpacity: 0.3,
-      strokeColor: "#ff0000",
-      strokeOpacity: 0.6,
-      strokeWeight: 3,
-    });
-
-    var polyline = new naver.maps.Polyline({
-      map: map,
-      path: [
-        new naver.maps.LatLng(37.359924641705476, 127.1148204803467),
-        new naver.maps.LatLng(37.36343797188166, 127.11486339569092),
-        new naver.maps.LatLng(37.368520071054576, 127.11473464965819),
-        new naver.maps.LatLng(37.3685882848096, 127.1088123321533),
-        new naver.maps.LatLng(37.37295383612657, 127.10876941680907),
-        new naver.maps.LatLng(37.38001321351567, 127.11851119995116),
-        new naver.maps.LatLng(37.378546827477855, 127.11984157562254),
-        new naver.maps.LatLng(37.376637072444105, 127.12052822113036),
-        new naver.maps.LatLng(37.37530703574853, 127.12190151214598),
-        new naver.maps.LatLng(37.371657839593894, 127.11645126342773),
-        new naver.maps.LatLng(37.36855417793982, 127.1207857131958),
-      ],
+    naver.maps.Event.addListener(map, "click", function (e) {
+      marker.setPosition(e.latlng);
     });
   }, [draggable, zoomControl, keyboardShortcuts, mapTypeId]);
 
